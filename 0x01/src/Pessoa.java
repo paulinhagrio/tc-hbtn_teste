658a6e4373f97d8480324a40d80720222fd70918
Paula Guimaraes <paulinhagrio@hotmail.com>
2022-06-10 10:38:03 -0300

    Email Junit

---
+++ 0x01/src/Pessoa.java
@@ -0,0 +1,13 @@
+package src;
+
+
+public class Pessoa {
+    public static boolean emailValid(String email){
+        if (email.contains("@")){
+            if(email.length() <= 50){
+                return true;
+            }
+        }
+        return false;
+    }
+}
